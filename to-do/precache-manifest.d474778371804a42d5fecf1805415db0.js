3c5df776258567a0d63a4e5d6fb47399aaaad786
niko_lie@mail.ru <Melbravo544611>
2020-06-22 22:06:13 +0300

    upd

---
+++ to-do/precache-manifest.d474778371804a42d5fecf1805415db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072d55be920d08d9a0866a6ed196cd6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2491691bfaa429bffbd",
+    "url": "/static/css/main.898beed5.chunk.css"
+  },
+  {
+    "revision": "5600a472cbe2ed3d4d88",
+    "url": "/static/js/2.265b008c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.265b008c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2491691bfaa429bffbd",
+    "url": "/static/js/main.575f2335.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
